f60b2e264263c4f761104fecbd407c5efb839b40
Tricia-campos <triciacamposm@gmail.com>
2024-09-03 19:23:07 -0300

    atvd

---
+++ atv_home.py
@@ -0,0 +1,16 @@
+print("RECIBO")
+cliente = input("Digite seu nome: ")
+produto = input("Você irá abastecer com (E) etanol ou (G) gasolina? ")
+litros = (float(input(f"Quantos litros de você vai abastecer? ")))
+
+
+if produto == "E" or produto == "e":
+    resultado = litros *  4.10
+    print(f"O {cliente} abasteceu com etanol o total foi:{resultado}")
+elif produto == "G" or produto == "g":
+    resultado = litros * 5.8
+    print(f"O {cliente} abasteceu com gasolina o total foi:{resultado}")
+else:
+    print("invalido")
+
+
